b8f863089d8a9f621f4382bb8c56ba55ab13c7b8
Dipak <125576159+DipakShinde1196@users.noreply.github.com>
2024-10-13 23:27:25 +0530

    Update Rectangle.py

--- Rectangle.py
+++ Rectangle.py
@@ -2,15 +2,11 @@ class Rectangle:
     def __init__(self, length: int, width: int):
         self.length = length
         self.width = width
-
-    # Custom iterator
     def __iter__(self):
         # Yielding the length first, followed by the width
         yield {'length': self.length}
         yield {'width': self.width}
 
-# Example usage
 rect = Rectangle(10, 5)
-
 for dimension in rect:
     print(dimension)
